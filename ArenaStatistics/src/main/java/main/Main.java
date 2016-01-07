a769a4f1f8abb0b2fe3e6397a3b98ef0e722c44c
eerojala <eero.ojala@helsinki.fi>
2016-01-07 16:18:55 +0200

    Project (hopefully) in working condtion -- added executable jar file

---
+++ ArenaStatistics/src/main/java/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import logic.StartUp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StartUp startUp = new StartUp();
+        startUp.Start();
+    }
+}
